3dc664592d80b91f3b62965011fd2a56982d63f5
Shreyansh Pandey <s@isomr.co>
2018-12-08 03:14:17 +0530

    added mocker, fixtures and TwoFactor object

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -1,3 +1,8 @@
+const { TwoFactor } = require( '../../lib' );
+const Mocker = require( './mocker' );
+
 module.exports = {
-    
+    mocker: new Mocker( 'd3aa88e2-c754-41e0-8ba6-4198a34aa0a2' ),
+    Fixtures: require( './fixtures' ),
+    TwoFactor: new TwoFactor( 'd3aa88e2-c754-41e0-8ba6-4198a34aa0a2' )
 };
