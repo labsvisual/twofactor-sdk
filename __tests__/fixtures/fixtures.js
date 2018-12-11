0b465510efc2432e8935eac76c1882a7be244f61
Shreyansh Pandey <s@isomr.co>
2018-12-12 00:43:15 +0800

    added fixtures for testing the schema parser

--- __tests__/fixtures/fixtures.js
+++ __tests__/fixtures/fixtures.js
@@ -22,6 +22,18 @@ module.exports = {
         }
     },
 
-    guidRegexp: /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+    guidRegexp: /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i,
+
+    exampleSchema: {
+        $root: {
+            allowUndefined: false,
+            allowNull: false,
+            allowedTypes: [ 'string', 'object' ]
+        },
+        test: {
+            allowedTypes: 'string',
+            isOptional: true
+        }
+    }
 
 };
